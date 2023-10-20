b2697d72c7379552308c70b9ffb0b5087f97455d
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2023-10-20 23:03:10 +0600

    image responsive done

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from "react";
-import Carousel from "react-bootstrap/Carousel";
+import {Carousel} from "react-bootstrap";
 import carouselItemData from "../../Data/carouselItemData.json";
 
 const Hero = () => {
-  console.log(carouselItemData);
-  const [casouselItem, setCasouselItem] = useState([]);
+  // console.log(carouselItemData);
+  const [carouselItem, setCarouselItem] = useState([]);
   useEffect(() => {
-    setCasouselItem(carouselItemData);
+    setCarouselItem(carouselItemData);
   }, []);
   return (
     <div>
       <Carousel>
-        {casouselItem.slice(0,3).map((citem) => (
+        {carouselItem.slice(0, 3).map((citem) => (
           <Carousel.Item key={citem.key}>
-            
-            <img src={citem.img} style={{width: '1400px' , height: '450px'}} alt="" />
-
+            <img 
+              src={citem.img}
+              className="img-fluid"
+              style={{ width: "1400px", height: "450px"  }}
+              alt=""
+            />
           </Carousel.Item>
         ))}
       </Carousel>
