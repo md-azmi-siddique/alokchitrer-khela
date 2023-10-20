48c89c3cc3176c3ea47c3b334636c94bf578b374
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2023-10-20 15:25:18 +0600

    navbar added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,32 +1,68 @@
 import React from "react";
 import logo from "../../Assets/logo.jpg";
-import "../Header/Header.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import './Header.css'
 
-const Header = () => {
+function Header() {
   return (
-    <div className="header">
-      <div>
-        <img src={logo} alt="" />
-      </div>
-
-      <div className="search-box">
-        <input type="text" placeholder="Search..." />
-      </div>
-
-      <div className="createAccount">
-        <div className="userAccountIcon">
-          <h2>
-            <FontAwesomeIcon icon={faUser} />
-          </h2>
-        </div>
-        <div className="userAccountName">
-          <h2>My Account</h2>
-        </div>
-      </div>
+    
+    <div className="container">
+      {['sm'].map((expand) => (
+        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#"><img src={logo} alt="" /></Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">Link</Nav.Link>
+                  <NavDropdown
+                    title="Dropdown"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Form className="d-flex total-search-box">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2 "
+                    aria-label="Search"
+                    style={{ width: '500px' }}
+                  />
+                  <Button variant="outline-success" className="d-flex">Search</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
     </div>
   );
-};
+}
 
 export default Header;
