bccdae62c30ec7bc9475433e6fca0873f3691f27
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2023-10-31 20:27:13 +0600

    nabvar color and offCanvas changed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,43 +7,29 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
-
 function Header() {
-
   return (
     <div className="header">
       {["md"].map((expand) => (
-        <Navbar 
-        fixed="top"
-          data-bs-theme="dark"
-          key={expand}
-          expand={expand}
-          className="custom-navbar"
-          id="navBar"
-        >
+        <Navbar fixed="top" data-bs-theme="dark" key={expand} expand={expand} className="custom-navbar" id="navBar">
           <Container fluid>
             <Navbar.Brand>
               <img src={logo} alt="" />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
+            <Navbar.Offcanvas className="custom-offCanvas" id={`offcanvasNavbar-expand-${expand}`} aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`} placement="end">
               <Offcanvas.Header closeButton>
-                <Offcanvas.Title
-                  id={`offcanvasNavbarLabel-expand-${expand}`}
-                ></Offcanvas.Title>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  <img src={logo} alt="" />
+                </Offcanvas.Title>
               </Offcanvas.Header>
-              <Offcanvas.Body>
+              <Offcanvas.Body className="custom-offcanvas-body">
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link><Link className="header-item" to="/">Home</Link></Nav.Link>
-                  <Nav.Link><Link className="header-item" to="/FeaturedProducts">Featured Products</Link></Nav.Link>
+                  <Nav.Link><Link className="header-item custom-link" to="/">Home</Link></Nav.Link>
+                  <Nav.Link><Link className="header-item custom-link"to="/FeaturedProducts">Featured Products</Link></Nav.Link>
                 </Nav>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
-          
           </Container>
         </Navbar>
       ))}
